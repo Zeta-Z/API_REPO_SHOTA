ef225ec90a4c38d5b4a5219999e82ba75611be10
Zeta-Z <suescumperea@gmail.com>
2023-05-08 20:32:34 -0500

    V1.1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypts = require("bcryptjs");
+const { use } = require("../routes/api/user");
 const db = require("C:/Users/zeta/Desktop/API_Port/models");
 const jwt = require("jsonwebtoken");
 
@@ -58,6 +59,71 @@ exports.listOne = async (req, res) => {
   }catch(error){
     res.status(500).send({
       error: "Error en el servidor! \n "+ error,
+    });
+  }
+
+};
+
+exports.delete = async (req, res) => {
+
+  try{
+    const userEmail = req.params.email;
+    const Usuario = await db.User.findOne({where:{email: userEmail }});
+
+    if(Usuario){
+      const dUser = await db.User.destroy({
+        where:{
+          email: userEmail,
+        }
+      })
+
+      res.status(200).send({
+        message: "Usuario eliminado con éxito.",
+      });
+
+    }else{
+      res.status(404).send({
+        error: 'No hay registros en el sistema.'
+    });
+    }
+  }catch(error){
+    res.status(500).send({
+      error: "Error en el servidor! \n" + error,
+    });
+  }
+
+};
+
+exports.update = async (req,res) => {
+  try{
+
+    const email = req.params.email;
+
+    const user = await db.User.findOne({where:{email : email}});
+
+    if(user){
+
+      let username = (req.body.username != undefined || req.body.username != null ) ? req.body.username : user.username;
+      let cellphone = (req.body.cellphone != undefined || req.body.cellphone != null) ? req.body.cellphone : user.cellphone;
+
+      const UpUser = await db.User.update({
+        username : username,
+        cellphone, cellphone,
+      },{where: {email: email}});
+
+      res.status(200).send({
+        message: "Usuario actualizado con éxito.",
+      });
+    }else{
+      res.status(404).send({
+        error: 'No hay registros en el sistema.'
+    });
+    }
+    //let Question2 = (req.body.Question2 != undefined || req.body.Question2 != null) ? req.body.Question2 : 'N/A';
+
+  }catch(error){
+    res.status(500).send({
+      error: "Error en el servidor! \n " + error,
     });
   }
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -5,5 +5,8 @@ const userController = require('C:/Users/zeta/Desktop/API_Port/controllers/userC
 router.post('/create',userController.add);
 router.get('/list',userController.list);
 router.get('/listOne/:email', userController.listOne);
+router.delete('/delete/:email', userController.delete);
+router.put('/update/:email', userController.update);
+
 
 module.exports = router;
